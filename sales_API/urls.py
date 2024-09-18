42eeb002e9ba77e4bbc8bd5decc31d3baca06bc6
homemix <kenmutati@gmail.com>
2024-09-18 08:45:19 +0300

    EDIT: users auth urls format.

--- sales_API/urls.py
+++ sales_API/urls.py
@@ -39,11 +39,9 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/auth/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/auth/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/customers/', include('customers.urls')),
     path('api/orders/', include('orders.urls')),
-    path('api/users/', include('users.urls')),
+    path('api/auth/', include('users.urls')),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),  # Swagger UI
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),  # ReDoc UI
 ]
--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,14 @@
-from django.urls import path
+from django.urls import path, include
 from .views import UserCreateView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
+    path('', include('dj_rest_auth.urls')),  # Login, Logout, Password Reset
     path('register/', UserCreateView.as_view(), name='user-register'),
+    # path('registration/', include('dj_rest_auth.registration.urls')),  # User Registration
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
