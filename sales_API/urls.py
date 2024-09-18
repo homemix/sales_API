fc236344a2953432221d5ec84f5d5eb1322c8e4c
homemix <kenmutati@gmail.com>
2024-09-18 08:38:20 +0300

    ADD: api documentation with swagger and redoc.

--- sales_API/urls.py
+++ sales_API/urls.py
@@ -15,11 +15,26 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 from django.urls import path, include
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
+)
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Sales API schema",
+        default_version='v1',
+        description="A Django rest frame work Sales API schema",
+        terms_of_service="https://www.example.com/terms/",
+        contact=openapi.Contact(email="kenmutati@gmail.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
 )
 
 urlpatterns = [
@@ -29,4 +44,6 @@ urlpatterns = [
     path('api/customers/', include('customers.urls')),
     path('api/orders/', include('orders.urls')),
     path('api/users/', include('users.urls')),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),  # Swagger UI
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),  # ReDoc UI
 ]
