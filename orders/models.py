a00295bda09d0f8c979d3f845da787b0a21a342a
homemix <kenmutati@gmail.com>
2024-09-18 08:12:27 +0300

    ADD: Orders app view, model and url.

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from customers.models import Customer
 
-# Create your models here.
+
+class Order(models.Model):
+    item = models.CharField(max_length=255)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    time = models.DateTimeField(auto_now_add=True)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE, related_name='orders')
+
+    def __str__(self):
+        return f"Order {self.id} for {self.item} by {self.customer.name}"
