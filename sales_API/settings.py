0d213bc43332c17fa44675adb3bcee09225fc759
homemix <kenmutati@gmail.com>
2024-09-20 18:20:28 +0300

    UPDATE: token time delat for refresh.

--- sales_API/settings.py
+++ sales_API/settings.py
@@ -151,13 +151,6 @@ REST_FRAMEWORK = {
     ]
 }
 
-# SIMPLE_JWT = {
-#     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
-#     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
-#     'ROTATE_REFRESH_TOKENS': True,
-#     'BLACKLIST_AFTER_ROTATION': True,
-#     'AUTH_HEADER_TYPES': ('Bearer',),
-# }
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
@@ -168,7 +161,7 @@ CORS_ALLOWED_ORIGINS = [
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
     "ROTATE_REFRESH_TOKENS": False,
     "BLACKLIST_AFTER_ROTATION": False,
